243321e7bbe0b2071cc0a08a53e89aa323ca50d6
barghouthf <barghouth@gmail.com>
2024-02-24 15:29:39 +0300

    fix npm install

--- src/configs/dbConnect.js
+++ src/configs/dbConnect.js
@@ -6,7 +6,7 @@ export async function connectToDatabase() {
     'mongodb+srv://umanoinfo00:B4l0jIYvEdtOzNDE@umanosing.6seqopc.mongodb.net/UmanoDB?retryWrites=true&w=majority'
 
     // 'mongodb+srv://localhost/'
-    // 'mongodb://127.0.0.1:27017/umano?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.4'
+    // 'mongodb://127.0.0.1:27017/hr?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.4'
     
 
   )
