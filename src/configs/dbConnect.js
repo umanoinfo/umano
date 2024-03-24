b810855bc91bee822429b56a80a77722f929954b
barghouthf <barghouthf@gmail.com>
2024-03-24 09:43:32 +0300

    merg

--- src/configs/dbConnect.js
+++ src/configs/dbConnect.js
@@ -7,8 +7,7 @@ export async function connectToDatabase() {
 
     // 'mongodb+srv://localhost/'
     // 'mongodb://127.0.0.1:27017/hr?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.4'
-    
-
+    //
   )
   setTimeout(() => {
     client.close()
