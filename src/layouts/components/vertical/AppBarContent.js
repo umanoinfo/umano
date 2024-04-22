79c88fbd8133bd3093df123d1ee616c052e2e5be
Rami-Zidan <ramizidan101@gmail.com>
2024-04-22 13:47:50 +0300

    leave company

--- src/layouts/components/vertical/AppBarContent.js
+++ src/layouts/components/vertical/AppBarContent.js
@@ -3,6 +3,10 @@ import { Typography } from '@mui/material'
 import Box from '@mui/material/Box'
 import IconButton from '@mui/material/IconButton'
 import { useSession } from 'next-auth/react'
+import { Button } from '@mui/material'
+import { useState } from 'react'
+import axios from 'axios'
+import toast from 'react-hot-toast'
 
 // ** Icon Imports
 import Icon from 'src/@core/components/icon'
@@ -17,7 +21,34 @@ const AppBarContent = props => {
   const { hidden, settings, saveSettings, toggleNavVisibility } = props
 
   const { data: session, status } = useSession()
+  const [ loading , setLoading ] = useState();
 
+  const handleSubmit = ()=>{
+   
+      setLoading(true) ;
+    
+      axios
+        .post('/api/user/change-company', {
+          visit: false
+        })
+        .then(function (response) { 
+            toast.success('Leaving company succeded', {
+              duration: 3000,
+              position: 'bottom-right'
+            })
+            setLoading(false) ;
+            location.reload();
+        })
+        .catch(function (error) {
+          console.log(error);
+          toast.error('Error : ' + error.response.data.message + ' !', {
+            duration: 1000,
+            position: 'bottom-right'
+          })
+          setLoading(false)
+        })
+
+  }
 
   return (
     <Box sx={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
@@ -28,8 +59,13 @@ const AppBarContent = props => {
           </IconButton>
         ) : null}
 
-        {session.user.company_info && session.user.type == 'admin' && <Typography variant="h7"  sx={{color:'#c3c3c3' , fontStyle:'italic'}}>
+        {session.user?.company_info?.[0]?.name && session.user.type == 'admin' && <Typography variant="h7"  sx={{color:'#c3c3c3' , fontStyle:'italic'}}>
           Your Company is : <span style={{color:'blue' , fontWeight:'600'}}>{session.user.company_info[0]?.name}</span>
+          {
+            <div>
+              <Button onClick={handleSubmit}> leave company </Button>
+            </div>
+          }
         </Typography>}
        
       </Box>
