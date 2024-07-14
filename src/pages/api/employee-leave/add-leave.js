769019e0549eedb5b3817d0cbd14d43c320d63c7
Rami-Zidan <ramizidan101@gmail.com>
2024-07-14 16:11:27 +0400

    leave

--- src/pages/api/employee-leave/add-leave.js
+++ src/pages/api/employee-leave/add-leave.js
@@ -40,9 +40,9 @@ export default async function handler(req, res) {
   employeeLeave.status = 'active'
   employeeLeave.date_from_1 = employeeLeave.date_from ;
   employeeLeave.date_to_1 = employeeLeave.date_to ;
-  employeeLeave.date_from = new Date( employeeLeave.date_from).setHours(0,0,0,0)  // solves the problem of making it less one day
-  employeeLeave.date_to = new Date(employeeLeave.date_to).setHours(0,0,0,0)  // solves the problem of making it less one day
-
+  employeeLeave.date_from = new Date( new Date( employeeLeave.date_from).setUTCHours(23,59,59,999) ) // solves the problem of making it less one day
+  employeeLeave.date_to = new Date(new Date(employeeLeave.date_to).setUTCHours(23,59,59,999))  // solves the problem of making it less one day
+  
   const newEmployeeLeave = await client.db().collection('employeeLeaves').insertOne(employeeLeave)
 
   const insertedLeave = await client.db().collection('employeeLeaves').findOne({ _id: newEmployeeLeave.insertedId })
--- src/pages/api/employee-leave/edit-leave.js
+++ src/pages/api/employee-leave/edit-leave.js
@@ -27,8 +27,8 @@ export default async function handler(req, res) {
   }
 
 
-  employeeLeave.date_from = employeeLeave.date_from
-  employeeLeave.date_to = employeeLeave.date_to
+  employeeLeave.date_from = new Date( new Date( employeeLeave.date_from).setHours(23,59,59,999)) ;
+  employeeLeave.date_to = new Date( new Date(employeeLeave.date_to).setHours(23,59,59,999));
 
   if (
     !employeeLeave.reason ||
