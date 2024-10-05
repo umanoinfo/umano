3e4b839b1b3e7f1a86cf8f27953f6166593927dd
Rami-Zidan <ramizidan101@gmail.com>
2024-10-05 16:35:10 +0300

    fix : option retry/writes is not supported ! # 2

--- src/configs/dbConnect.js
+++ src/configs/dbConnect.js
@@ -4,13 +4,10 @@ export async function connectToDatabase() {
   const client = await MongoClient.connect(
 
 
-    'mongodb+srv://umanoinfo00:B4l0jIYvEdtOzNDE@umanosing.6seqopc.mongodb.net/UmanoDB?retry/Writes=true&w=majority'
+    'mongodb+srv://umanoinfo00:B4l0jIYvEdtOzNDE@umanosing.6seqopc.mongodb.net/UmanoDB?retry/Writes=false&w=majority'
 
     // 'mongodb://127.0.0.1:27017/umano?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.4'
-    , 
-    {
-      retry_writes: false
-    }
+  
   )
 
   setTimeout(() => {
