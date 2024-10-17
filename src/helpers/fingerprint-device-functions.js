03a674e189dff7cdc31b41babb72f33da547fcdf
Rami-Zidan <ramizidan101@gmail.com>
2024-10-17 20:53:34 +0400

    date fix

--- src/helpers/fingerprint-device-functions.js
+++ src/helpers/fingerprint-device-functions.js
@@ -54,15 +54,16 @@ export const functions = {
         // times contains all the times for that employee in that date 
         let [idNo, date] = key.split('_');
         date = date.replaceAll('-', '/');
+
         console.log(date, "*******", times[0], "*******", new Date(date + 'T' + times[0] + 'Z'), "*******", new Date(date + '' + times[0]).toLocaleString(), "*******", new Date(date + 'T' + times[0] + 'Z').toLocaleString())
 
-        let mn = new Date(date + '' + times[0]).toLocaleString() //new Date(date + ' ' + times[0] + ' UTC'); ///new Date(date + 'T' + value[0] + 'Z')
-        let mx = new Date(date + '' + times[0]).toLocaleString() //new Date(date + ' ' + times[0] + ' UTC');  //// new Date(date + 'T' + value[0] + 'Z')
+        let mn = new Date(date + ' ' + times[0]) //new Date(date + ' ' + times[0] + ' UTC'); ///new Date(date + 'T' + value[0] + 'Z')
+        let mx = new Date(date + ' ' + times[0]) //new Date(date + ' ' + times[0] + ' UTC');  //// new Date(date + 'T' + value[0] + 'Z')
 
         mn = new Date(mn.getTime() + Math.abs(mn.getTimezoneOffset() * 60000))
         mx = new Date(mx.getTime() + Math.abs(mx.getTimezoneOffset() * 60000))
         console.log('This is what is stored ' , mn  ) ;
-
+        ``
         if (mn == 'Invalid Date' || mx == 'Invalid Date') {
           toast.error('Invalid Date format in your file please correct it to be like following: 1970-01-01');
 
